b7c873ade30a3630e706074d2dd65699b216cd62
Mathias Hauser <mathias.hauser@env.ethz.ch>
2018-06-18 19:28:13 +0200

    hide solutions

---
+++ Part0/button.py
@@ -0,0 +1,38 @@
+from IPython.display import HTML
+from IPython.display import display
+
+def add_buttons_to_toggle_visibility():
+
+    tag = HTML('''
+    
+    <script>
+    //$( document ).ready(function() {
+        
+    var add_button = function () {
+        Jupyter.notebook.get_cells().forEach(function(cell) {
+            if (cell.element.find("form.bla").length == 0) {
+                cell.element.find("div.output_wrapper").append('<form class="bla" action="javascript:toggle_selected_input()"><input type="submit" style="float: right;" value="T"></i></form>');
+            }
+        })
+    };
+    
+    var toggle_selected_input = function () {
+        // Find the selected cell
+        var cell = Jupyter.notebook.get_selected_cell();
+        // Toggle visibility of the input div
+        cell.element.find("div.input").toggle('slow');
+        cell.metadata.hide_input = ! cell.metadata.hide_input;
+        };
+    
+    
+    add_button()    
+
+      
+    //} );
+    </script>
+    
+
+
+    ''')
+    
+    return display(tag)
